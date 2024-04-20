b991d753e9cd09852eb730baa6933c5c576760c8
Moisés Gonçalves Silva <moises50hp@hotmail.com>
2024-04-20 19:38:01 -0300

    TokenService

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog;
+
+public static class Configuration
+{
+    //Token - JWT
+    public static string JwtKey { get; set; } = "92740520-786A-48DC-A64F-AD7523C42489";
+}
---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class AccountController(TokenService tokenService) : ControllerBase
+    {
+        private readonly TokenService _tokenService = tokenService;
+
+        [HttpPost("v1/login")]
+        public IActionResult Login()
+        {
+            var token = _tokenService.GenerateToken(null);
+
+            return Ok(token);
+        }
+    }
+}
