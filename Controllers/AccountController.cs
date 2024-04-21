1887c8aaf18453373462502e2d6e3128b30d4cf3
Moisés Gonçalves Silva <moises50hp@hotmail.com>
2024-04-21 19:05:02 -0300

    Salvando senhas

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,19 +1,65 @@
-﻿using Blog.Services;
+﻿using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
+using Blog.Services;
+using Blog.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace Blog.Controllers
 {
     [ApiController]
-    public class AccountController(TokenService tokenService) : ControllerBase
+    public class AccountController : ControllerBase
     {
-        private readonly TokenService _tokenService = tokenService;
+        [HttpPost("v1/accounts")]
+        public async Task<IActionResult> Post(
+            [FromBody] RegisterViewModel model,
+            [FromServices] BlogDataContext context
+            )
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace("@", "-").Replace(".", "-")
+            };
+
+            var password = PasswordGenerator.Generate(25);
+            user.PasswordHash = PasswordHasher.Hash(password);
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    user = user.Email,
+                    password
+                }));
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(400, new ResultViewModel<string>("05X99 - Este E-mail já está cadastrado"));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<string>("05X04 - Falah interna do servidor"));
+            }
+        }
 
         [HttpPost("v1/login")]
-        public IActionResult Login()
+        public IActionResult Login([FromServices] TokenService tokenService)
         {
-            var token = _tokenService.GenerateToken(null);
+            var token = tokenService.GenerateToken(null);
 
             return Ok(token);
         }
+
     }
 }
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -18,7 +18,9 @@ namespace Blog.Services
                 Subject = new ClaimsIdentity(new Claim[]
                 {
                     new (ClaimTypes.Name, "moises"), //User.Identity.Name
-                    new (ClaimTypes.Role, "admin") //User.IsInRole
+                    new (ClaimTypes.Role, "admin"), //User.IsInRole
+                    new (ClaimTypes.Role, "user"), //User.IsInRole
+                    new (ClaimTypes.Role, "author") //User.IsInRole
                 }),
                 Expires = DateTime.UtcNow.AddMinutes(20),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
